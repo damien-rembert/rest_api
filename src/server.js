b494a06078146f657aa92bd06afd23288adbc65a
Damien R <damien.rembert@gmail.com>
2022-03-15 14:20:56 +0000

    back-end and front-end working together

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("./db/connection");
 
 const express = require("express");
+const cors = require("cors");
 const movieRouter = require("./movie/movieRoutes");
 const userRouter = require("./user/userRoutes");
 const app = express();
@@ -9,7 +10,7 @@ const port = 5001;
 
 // app will use its .json() method to parse everything it receives as it will be json
 app.use(express.json());
-
+app.use(cors());
 app.use(movieRouter);
 app.use(userRouter);
 
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,12 +1,23 @@
 const { Router } = require("express");
-const { addUser } = require("./userControllers");
-const { hashPassword } = require("../middleware");
-const userRouter = Router();
+// const { addUser } = require("./userControllers");
+// const { hashPassword } = require("../middleware");
+// const userRouter = Router();
 
-userRouter.post("/user", hashPassword, addUser);
+// userRouter.post("/user", hashPassword, addUser);
 
 // use compare and check password
 
+
+const { addUser, login, updatePassword } = require("./userControllers");
+const { hashPassword, decryptPassword, checkToken } = require("../middleware");
+const userRouter = Router();
+
+userRouter.post("/user", hashPassword, addUser);
+userRouter.post("/login", decryptPassword, login);
+userRouter.get("/user", checkToken, login);
+userRouter.patch("/user", hashPassword, checkToken, updatePassword);
+
+module.exports = userRouter;
 
 
 module.exports = userRouter;
